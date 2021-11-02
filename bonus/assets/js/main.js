10a059f85ab29e8f0f3b112eb60f82d43427b08f
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-02 17:00:12 +0100

    aggiunto alert

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -40,6 +40,7 @@ const app = new Vue ({
         addTask(){
             if (this.newTask.length > 5) {
                 this.tasks.push(this.newTask);
+                alert(`Task updated! the new task is: ${this.newTask}`)
                 this.newTask = "";
                 this.error = false;
             } else {
@@ -72,6 +73,12 @@ const app = new Vue ({
                 this.discarica =false
             }
             console.log(this.discarica);
+        },
+
+        recycle(i){
+            let recycling = this.trashedTasks[i];
+            this.trashedTasks.splice(i,1);
+            this.tasks.push(recycling);
         }
     }
 })
@@ -105,3 +112,5 @@ const app = new Vue ({
 //finite le modifiche alla funzione removeTask (aggiunto push nell'array trashed), ho inserito l'icona check e ora creo la sua funzione
 //fatta funzione completeTask, ora passo alla box completed dove devo dargli il comando e la funzione per rifare il task (e quindi pusharlo di nuovo in tasks e rimuoverlo dai completati)
 //stessa cosa, creo funzione clearTask per rimuovere il task dal cestino e aggiungerlo in un ulteriore array per ultimo punto bonus (Il nostro utente per ora sembra non avere altre richieste ... ma chissá se dopo gli viene in mente che vuole anche poter rimettere una task cancellata nella lista di tasks da fare, magari l'ha cancellata per sbaglio?)
+
+//avevo male interpretato l'ultimo punto, ma oramai l'ho fatto in quel modo e lo lascio così. Aggiungo alert e prompt come da foto.
