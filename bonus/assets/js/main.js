97bb63ecfbd3612bd8bb090f4fae2ca80e1468b1
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-02 16:42:15 +0100

    fatte funzioni base e dump

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -20,6 +20,11 @@ const app = new Vue ({
 
         newTask:"",
 
+        dump:[
+        ],
+
+        discarica: false,
+
         error: false,
     },
 
@@ -32,7 +37,7 @@ const app = new Vue ({
             console.log(trashedTasks);
         },
 
-        addTask (){
+        addTask(){
             if (this.newTask.length > 5) {
                 this.tasks.push(this.newTask);
                 this.newTask = "";
@@ -41,6 +46,33 @@ const app = new Vue ({
                 this.error = true;
             }
         },
+
+        completeTask(i){
+            let complete = this.tasks[i];
+            this.tasks.splice(i,1);
+            this.completedTasks.push(complete);
+        },
+
+        redoTask(i){
+            let redo = this.completedTasks[i];
+            this.completedTasks.splice(i,1);
+            this.tasks.push(redo);
+        },
+
+        clearTask(i){
+            let clear = this.trashedTasks[i];
+            this.trashedTasks.splice(i,1);
+            this.dump.push(clear);
+        },
+
+        showDumpster(){
+            if (this.discarica == false) {
+                this.discarica = true;
+            } else {
+                this.discarica =false
+            }
+            console.log(this.discarica);
+        }
     }
 })
 
@@ -70,3 +102,6 @@ const app = new Vue ({
 //creati array con elemento default per test
 //ora devo ripetere la procedura che ho utilizzato nel task per creare i div, ovvero il ciclo v-for
 //create le box, devo aggiungere il tick per il completamento e rimaneggiare i methods per far sì che i vari click pushino il task nell'array che voglio
+//finite le modifiche alla funzione removeTask (aggiunto push nell'array trashed), ho inserito l'icona check e ora creo la sua funzione
+//fatta funzione completeTask, ora passo alla box completed dove devo dargli il comando e la funzione per rifare il task (e quindi pusharlo di nuovo in tasks e rimuoverlo dai completati)
+//stessa cosa, creo funzione clearTask per rimuovere il task dal cestino e aggiungerlo in un ulteriore array per ultimo punto bonus (Il nostro utente per ora sembra non avere altre richieste ... ma chissá se dopo gli viene in mente che vuole anche poter rimettere una task cancellata nella lista di tasks da fare, magari l'ha cancellata per sbaglio?)
